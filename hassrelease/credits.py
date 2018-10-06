3ba963ffefecc23c1098711cde31d75f235d992e
WofWca <39462442+WofWca@users.noreply.github.com>
2018-10-07 00:01:10 +0700

    Delete the old generator, add the new (not completed)

--- hassrelease/credits.py
+++ hassrelease/credits.py
@@ -1,5 +1,11 @@
 from .github import get_session
+from .const import GITHUB_ORGANIZATION_NAME
 
 
 def generate_credits():
     gh = get_session()
+    org = gh.organization(GITHUB_ORGANIZATION_NAME)
+    for repo in org.repositories(type='public'):
+        for contributor in repo.contributors(anon=True):
+            print(contributor.login + '\t' + str(contributor.contributions))
+        print(repo.ratelimit_remaining)
